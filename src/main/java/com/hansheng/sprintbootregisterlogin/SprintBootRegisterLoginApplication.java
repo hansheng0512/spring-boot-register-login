16eb2f8b8b938c4cb1df80f69b6fe622841df7fe
Han Sheng <hanshengliang@outlook.com>
2022-02-11 18:13:43 +0800

    :tada: Init

---
+++ src/main/java/com/hansheng/sprintbootregisterlogin/SprintBootRegisterLoginApplication.java
@@ -0,0 +1,13 @@
+package com.hansheng.sprintbootregisterlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootRegisterLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootRegisterLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hansheng/sprintbootregisterlogin/SprintBootRegisterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansheng.sprintbootregisterlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootRegisterLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
