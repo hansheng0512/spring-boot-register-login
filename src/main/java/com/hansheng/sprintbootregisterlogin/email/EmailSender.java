fa78aad48e88dcc1bb3f3422dcb3578e8837d70c
Han Sheng <hanshengliang@outlook.com>
2022-02-13 14:00:15 +0800

    :zap: Send Email

---
+++ src/main/java/com/hansheng/sprintbootregisterlogin/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.hansheng.sprintbootregisterlogin.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
--- src/main/java/com/hansheng/sprintbootregisterlogin/registration/token/ConfirmationTokenRepository.java
+++ src/main/java/com/hansheng/sprintbootregisterlogin/registration/token/ConfirmationTokenRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -14,6 +15,7 @@ public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationT
     Optional<ConfirmationToken> findByToken(String confirmationToken);
 
     @Modifying
+    @Transactional
     @Query("UPDATE ConfirmationToken c SET c.confirmedAt = ?2 WHERE c.token = ?1")
     int updateConfirmedAt(String token, LocalDateTime confirmedAt);
 }
